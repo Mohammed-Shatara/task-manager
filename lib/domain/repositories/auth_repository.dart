7bb27f2df6804e6215127e464d3243e674e54811
Muhammed Shatara <muhashsyw123@gmail.com>
2025-05-19 22:46:43 +0300

    made some fixes on auth implementation

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -3,8 +3,9 @@ import 'package:task_manager/core/repository/repository.dart';
 import 'package:task_manager/core/result/result.dart';
 
 import '../../data/models/user_model.dart';
+import '../../data/requests/user_request.dart';
 
 abstract class AuthRepository extends Repository {
   Future<Result<BaseError, UserModel>> login(String email, String password);
-  Future<Result<BaseError, UserModel>> createUser(UserModel userModel);
+  Future<Result<BaseError, UserModel>> createUser(UserRequest userRequest);
 }
